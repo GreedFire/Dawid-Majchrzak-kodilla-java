46c5f8e8045e4fd9ae133a31bf5e06e4502fbdfa
GreedFire <greedofiro@gmail.com>
2019-04-26 10:24:46 +0200

    Observer Pattern v2

--- kodilla-patterns2/src/test/java/com/kodilla/patterns2/observer/homework/TaskListTestSuite.java
+++ kodilla-patterns2/src/test/java/com/kodilla/patterns2/observer/homework/TaskListTestSuite.java
@@ -8,17 +8,24 @@ public class TaskListTestSuite {
     @Test
     public void testUpdate(){
         //Given
-        Teacher teacher = new Teacher("Mentor");
-        TaskList tasklist = new TaskList("User1 tasklist");
+        Teacher teacher1 = new Teacher("Mentor1");
+        Teacher teacher2 = new Teacher("Mentor2");
+        TaskList tasklist1 = new TaskList("User1 tasklist");
+        TaskList taskList2 = new TaskList("User2 tasklist");
         Task task1 = new Task("task1");
         Task task2 = new Task("task2");
         Task task3 = new Task("task3");
-        tasklist.registerOvserver(teacher);
+        tasklist1.registerOvserver(teacher1);
+        taskList2.registerOvserver(teacher2);
         //When
-        tasklist.addTask(task1);
-        tasklist.addTask(task2);
-        tasklist.addTask(task3);
+        tasklist1.addTask(task1);
+        tasklist1.addTask(task2);
+        tasklist1.addTask(task3);
+        taskList2.addTask(task1);
+        taskList2.addTask(task2);
+
         //Then
-        Assert.assertEquals(3, teacher.getUpdateCount());
+        Assert.assertEquals(3, teacher1.getUpdateCount());
+        Assert.assertEquals(2, teacher2.getUpdateCount());
     }
 }
